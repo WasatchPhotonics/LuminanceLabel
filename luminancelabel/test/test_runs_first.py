8fa7b6200194c2b88fa199e741108a5a12e7e385
nharringtonwasatch <nharrington@wasatchphotonics.com>
2015-08-30 13:33:46 -0400

    test cleanup, documentation notes

--- luminancelabel/test/test_runs_first.py
+++ luminancelabel/test/test_runs_first.py
@@ -28,6 +28,9 @@ class Test(unittest.TestCase):
 
         A workaround is to run each test individually with:
         python -u -m unittest test_rotate.Test.test_XYZ
+
+        The real fix appears to be to move the QApplication call out of
+        the setup entirely, and put it at the module level.
         """
 
         app.closeAllWindows()
@@ -77,57 +80,6 @@ class Test(unittest.TestCase):
         wid_opac = self.form.windowOpacity()
         self.assertLess(wid_opac, 1.0) 
         self.assertGreater(wid_opac, 0.1) 
-
-    def test_create_window_components(self):
-        self.assertEqual(self.form.lblLuminance.text(), "Default")
-
-        # Designator needs to close itself after 1 second
-        QtTest.QTest.qWaitForWindowShown(self.form) 
-
-        # Verify that the window starts up 800 wide and 200 tall
-        self.assertEqual(self.form.width(), 800)
-        self.assertEqual(self.form.height(), 600)
-       
-        # List all svgwidgets, assumption that at least one is required
-        all_svg = self.form.findChildren(QtSvg.QSvgWidget)
-        self.assertTrue(len(all_svg) > 0)
-
-        # Make sure the svg widget is "hidden" size
-        svgWidget = all_svg[0]
-        self.assertEqual(svgWidget.width(), 0)
-        self.assertEqual(svgWidget.height(), 600)
-
-
-    def test_animate_components(self):
-
-        # Trigger the startup animation, make sure end state of svg
-        # widget is full extent dimensions
-        self.form.startup_animation()
-        QtTest.QTest.qWait(3000)
-
-        all_svg = self.form.findChildren(QtSvg.QSvgWidget)
-        self.assertTrue(len(all_svg) > 0)
-
-        svgWidget = all_svg[0]
-        self.assertEqual(svgWidget.width(), 800)
-    
-        # animated out side is not quite 600. On linux it is 576 and on
-        # ms windows it is 579. Make it a range for cross platform
-        # checks.
-        self.assertGreater(svgWidget.height(), 560)
-        self.assertLess(svgWidget.height(), 610)
-
-        #  !!!!!!!!!!!!!!!!!!!!! @@@@@@@@@@@@@@@@@@@@@@@               #
-        #
-        #  Don't wait for longer than the auto close delay. This leads
-        #  to failures of the nose. Seriously, it just quits with no
-        #  information printed. No "Ran X test..", no coverage summary,
-        #  no segfault info, nothing. Same for unittest. Leaving this in
-        #  here for a reminder
-        #
-        #  !!!!!!!!!!!!!!!!!!!!! @@@@@@@@@@@@@@@@@@@@@@@               #
-        QtTest.QTest.qWait(10)
-
 
     def test_create_window_components(self):
         self.assertEqual(self.form.lblLuminance.text(), "Default")
